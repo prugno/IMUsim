0fbad6659fa1ea5df7eda56dea7bfdd38279cac8
Angelo Mottolese <mottoleseangelo@gmail.com>
2017-12-04 10:55:52 +0100

    Initial Commit

---
+++ IMUsim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IMUsim
+//
+//  Created by uea on 04/12/17.
+//  Copyright © 2017 uea. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
